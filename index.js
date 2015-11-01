76a4ecd7187c9ec5a44c4579190e70b1049f267f
Jon Sadka <jonsadka@gmail.com>
2015-10-31 21:50:50 -0700

    Add margins to graph

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var maxCount = 0;
 var totalCount = 0;
+var incomeThresholds = [];
+var DEFAULT_SALARY = 300000;
 
 // ADD PAGE ELEMENTS
 var upperContainer = document.getElementById('upper-content');
@@ -18,36 +20,36 @@ var svg = d3.select('#chart').append('svg')
     .attr('transform', 'translate(' + leftAdjust + ',' + topAdjust + ')');
 
 // SETUP THE D3 EQUATIONS
-var xScale = d3.scale.linear().range([0, chartDimension]);
+var margin = {top: 20, right: 20, bottom: 20, left: 20};
+var xScale = d3.scale.linear().range([0, chartDimension - margin.right - margin.left]);
 var yScale = d3.scale.linear().range([chartDimension / 2, 0]);
-// var line = d3.svg.line().interpolate('cardinal')
-//   .x(function(d){return xScale( getIncomeIndex(d.income) )})
-//   .y(function(d){return yScale(d.count);})
 
 var area = d3.svg.area().interpolate('cardinal')
-  .x(function(d){return xScale(d.originalIndex);})
-  .y0(0)
+  .x(function(d){return xScale(d.originalIndex) + margin.left;})
+  .y0(0 + margin.top)
   .y1(function(d){return yScale(d.count);})
 
 d3.csv('data.csv', formatCSVData, processData);
 
-function formatCSVData(IrsCsv, index){
-  totalCount += +IrsCsv.count;
-  maxCount = Math.max(maxCount, IrsCsv.count);
+function formatCSVData(irsCsv, index){
+  totalCount += +irsCsv.count;
+  maxCount = Math.max(maxCount, irsCsv.count);
 
-  var threshold = IrsCsv.range.split(' to ') || IrsCsv.range;
+  var threshold = irsCsv.range.split(' to ') || irsCsv.range;
   var lowerThreshold = +threshold[0].slice(1).split(',').join('');
   var upperThreshold = +threshold[1].slice(1).split(',').join('');
   return {
-    text: IrsCsv.range.split(' to ')[0],
+    text: irsCsv.range.split(' to ')[0],
     lowerThreshold: lowerThreshold,
     upperThreshold: upperThreshold || Infinity,
-    count: +IrsCsv.count,
+    count: +irsCsv.count,
+    cumulCount: totalCount,
     originalIndex: index
   }
 }
 
 function processData(err, irsData){
+  incomeThresholds = irsData;
   console.log(totalCount)
   console.log(irsData)
 
@@ -74,7 +76,7 @@ function initialRender(irsData){
         .attr({
           'class': 'outline circle',
           'r': function(d){return xScale(d.originalIndex)/2;},
-          'cx': function(d){return xScale(d.originalIndex)/2;},
+          'cx': function(d){return xScale(d.originalIndex)/2 + margin.left;},
           'cy': chartDimension/2,
           'opacity': 0.20
         })
@@ -84,8 +86,8 @@ function initialRender(irsData){
       .attr({
         'class': 'salary circle',
         'fill': 'RGBA(0, 140, 112, ' + (0.5 + 0.5 * 0.3) + ')',
-        'r': function(d){return xScale(16.9)/2;},
-        'cx': function(d){return xScale(16.9)/2;},
+        'r': function(d){return xScale( calculateSalaryStats(DEFAULT_SALARY).index )/2;},
+        'cx': function(d){return xScale( calculateSalaryStats(DEFAULT_SALARY).index )/2 + margin.left;},
         'cy': chartDimension/2
       })
 
@@ -93,10 +95,10 @@ function initialRender(irsData){
     .append('rect')
       .attr({
         'class': 'cover white',
-        'x': 0,
-        'y': 0,
-        'width': chartDimension,
-        'height': chartDimension / 2,
+        'x': 0 + margin.left,
+        'y': 0 + margin.top,
+        'width': chartDimension - margin.left - margin.right,
+        'height': chartDimension / 2 - margin.top,
         'fill': 'white',
       })
 
@@ -104,10 +106,10 @@ function initialRender(irsData){
     .append('rect')
       .attr({
         'class': 'cover green',
-        'x': 0,
-        'y': 0,
-        'width': chartDimension,
-        'height': chartDimension / 2,
+        'x': 0 + margin.left,
+        'y': 0 + margin.top,
+        'width': chartDimension - margin.left - margin.right,
+        'height': chartDimension / 2 - margin.top,
         'fill': 'RGBA(0, 140, 112, 0.5)'
       })
 
@@ -115,10 +117,10 @@ function initialRender(irsData){
     .append('rect')
       .attr({
         'class': 'cover salary',
-        'x': 0,
-        'y': 0,
-        'width': function(d){return xScale(16.9);},
-        'height': chartDimension / 2,
+        'x': 0 + margin.left,
+        'y': 0 + margin.top,
+        'width': function(d){return xScale( calculateSalaryStats(DEFAULT_SALARY).index );},
+        'height': chartDimension / 2 - margin.top,
         'fill': 'RGBA(0, 140, 112, 0.5)'
       })
 
@@ -149,9 +151,9 @@ function initialRender(irsData){
       .append('line')
         .attr({
           'class': 'flag line',
-          'x1': function(d){return xScale(d.originalIndex);},
-          'x2': function(d){return xScale(d.originalIndex);},
-          'y1': 0,
+          'x1': function(d){return xScale(d.originalIndex) + margin.left;},
+          'x2': function(d){return xScale(d.originalIndex) + margin.left;},
+          'y1': 0 + margin.top,
           'y2': function(d){return yScale(d.count) - 15;}
         })
 
@@ -166,6 +168,30 @@ function initialRender(irsData){
         .attr({
           'class': 'flag label',
           'dy': '0.9em',
-          'transform': function(d){return 'translate('+ xScale(d.originalIndex) +',' + 0 + ')rotate(-90)';}
+          'transform': function(d){return 'translate('+ (xScale(d.originalIndex) + margin.left) +',' + (0 + margin.top) + ')rotate(-90)';}
         })
+}
+
+
+function calculateSalaryStats(userSalary){
+  // var result = {index: 123, percentage: .53};
+  for (var i = 0; i < incomeThresholds.length; i++){
+    var threshold = incomeThresholds[i];
+    if (userSalary < threshold.upperThreshold) {
+      var factor = (userSalary - threshold.lowerThreshold)/(threshold.upperThreshold - threshold.lowerThreshold);
+      var calculatedIndex = i + factor;
+      var calculatedCumulCount = threshold.cumulCount - factor * threshold.count
+
+      console.log(calculatedCumulCount / totalCount)
+      return {index: calculatedIndex, percentage: calculatedCumulCount / totalCount}
+    //   var citizenChange = factor*(threshold.citizens - threshold.citizens);
+    //   result.push({
+    //     income: userSalary,
+    //     citizens: threshold.citizens + citizenChange,
+    //     index: threshold.index + factor,
+    //     cumulCitizens: threshold.cumulCitizens + Math.abs(citizenChange)
+    //   });
+    };
+  }
+  return {};
 }
